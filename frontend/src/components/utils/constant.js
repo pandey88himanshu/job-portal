3e41aad18c9f6dc9b8f73d76f80f1564e6b1000f
pandey88himanshu <pandey88himanshu@gmail.com>
2024-12-21 02:04:25 +0530

    added backend live url

--- frontend/src/components/utils/constant.js
+++ frontend/src/components/utils/constant.js
@@ -1,5 +1,8 @@
-export const USER_API_END_POINT = "http://localhost:3000/api/v1/user";
-export const JOB_API_END_POINT = "http://localhost:3000/api/v1/job";
+export const USER_API_END_POINT =
+  "https://job-portal-rho-hazel.vercel.app/api/v1/user";
+export const JOB_API_END_POINT =
+  "https://job-portal-rho-hazel.vercel.app/api/v1/job";
 export const APPLICATION_API_END_POINT =
-  "http://localhost:3000/api/v1/application";
-export const COMPANY_API_END_POINT = "http://localhost:3000/api/v1/company";
+  "https://job-portal-rho-hazel.vercel.app/api/v1/application";
+export const COMPANY_API_END_POINT =
+  "https://job-portal-rho-hazel.vercel.app/api/v1/company";
