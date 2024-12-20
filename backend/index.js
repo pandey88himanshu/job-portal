c208db9d26c489358054f1eb513e3e9107874c51
pandey88himanshu <pandey88himanshu@gmail.com>
2024-12-21 02:20:55 +0530

    cors updated

--- backend/index.js
+++ backend/index.js
@@ -19,7 +19,7 @@ const corsOptions = {
   origin: process.env.ORIGIN_URL,
   credentials: true,
 };
-app.use(cors(corsOptions));
+app.use(cors());
 //api
 app.use("/api/v1/user", userRoute);
 app.use("/api/v1/company", companyRoutes);
